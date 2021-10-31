7936e9c475a08b237a456020f906e7a782c490f0
YaremaV <Yaremenko1996v@gmail.com>
2021-10-31 17:43:21 +0200

    Updates

--- static/js/main.368c23a7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpget_weather=this.webpackJsonpget_weather||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),i=(n(8),n(9),n(0));var o=function(){return Object(i.jsx)("div",{className:"App"})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.368c23a7.chunk.js.map
