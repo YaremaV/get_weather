f15979a77ca4ecfdea5b0ccab605fb13d6bef789
YaremaV <Yaremenko1996v@gmail.com>
2021-10-31 21:40:35 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import AppBar from "./component/AppBar/AppBar";
 import Container from "./component/Container/Container";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import Loader from "react-loader-spinner";
-import { HashRouter } from "react-router-dom";
 
 const SearchCountry = lazy(() => import("./views/Home/Home"));
 const Information = lazy(() => import("./views/Information/Information"));
@@ -13,45 +12,42 @@ const Information = lazy(() => import("./views/Information/Information"));
 export default function App() {
   return (
     <Container>
-      <HashRouter>
-        <AppBar />
+      <AppBar />
+      <Suspense
+        fallback={
+          <Loader
+            type="ThreeDots"
+            color="teal"
+            height={300}
+            width={300}
+            timeout={3000}
+          />
+        }
+      >
+        <Switch>
+          <Route path="/get_weather" exact>
+            <SearchCountry />
+          </Route>
 
-        <Suspense
-          fallback={
-            <Loader
-              type="ThreeDots"
-              color="teal"
-              height={300}
-              width={300}
-              timeout={3000}
-            />
-          }
-        >
-          <Switch>
-            <Route path="/get_weather" exact>
-              <SearchCountry />
-            </Route>
+          <Route path="/Information/:infoId">
+            <Information />
+          </Route>
 
-            <Route path="/Information/:infoId">
-              <Information />
-            </Route>
+          {/* <Route path="/Information/:infoId"></Route> */}
+        </Switch>
+      </Suspense>
 
-            {/* <Route path="/Information/:infoId"></Route> */}
-          </Switch>
-        </Suspense>
-
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </HashRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Container>
   );
 }
